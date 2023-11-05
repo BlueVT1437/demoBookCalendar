7779f874c270ac9d4179d917052d93a859f2f112
KietTran <kiettranvt1709@gmail.com>
2023-11-05 16:43:43 +0700

    init src code

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Paper from "@mui/material/Paper";
+import {
+  ViewState,
+  EditingState,
+  IntegratedEditing,
+} from "@devexpress/dx-react-scheduler";
+import {
+  Scheduler,
+  DayView,
+  Appointments,
+  ConfirmationDialog,
+  AppointmentTooltip,
+  AppointmentForm,
+  MonthView,
+} from "@devexpress/dx-react-scheduler-material-ui";
+import { schedulerData } from "./dummyData/appointment";
+
+interface IAppointment {
+  allDay: boolean;
+  endDate?: string;
+  startDate?: string;
+  title?: string;
+  rRule?: string;
+}
 
 function App() {
+  const [dataAppointment, setDataAppointment] = useState(schedulerData);
+
+  const setAppointment = (appointmentInfo: any) => {
+    console.log("startDate", new Date(appointmentInfo?.added.startDate));
+
+    const newAppointment = {
+      startDate: new Date(appointmentInfo?.added.startDate).toISOString(),
+      endDate: new Date(appointmentInfo?.added.endDate).toISOString(),
+      title: appointmentInfo.added.title,
+    };
+    // dataAppointment.push(newAppointment);
+    setDataAppointment((prev) => [...prev, newAppointment]);
+  };
+
+  const deleteAppointment = (val: any) => {
+		console.log('');
+	}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Paper>
+      <Scheduler data={dataAppointment} height={660}>
+        {/* <ViewState
+            currentDate={currentDate}
+          /> */}
+        <EditingState onCommitChanges={setAppointment} />
+        <IntegratedEditing />
+        {/* <DayView
+            startDayHour={9}
+            endDayHour={19}
+          /> */}
+        <MonthView />
+        <ConfirmationDialog />
+        <Appointments />
+        <AppointmentTooltip showOpenButton showDeleteButton />
+        <AppointmentForm />
+      </Scheduler>
+    </Paper>
   );
 }
 
---
+++ src/dummyData/appointment.ts
@@ -0,0 +1,12 @@
+export const schedulerData = [
+  {
+    startDate: "2023-11-05T09:45",
+    endDate: "2023-11-05T11:00",
+    title: "Meeting"
+  },
+  {
+    startDate: "2023-11-07T12:00",
+    endDate: "2023-11-07T13:30",
+    title: "Go to a gym"
+  },
+];
